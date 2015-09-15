1ff1b44580fb743109904f050b726dcb8e2e82cb
Pete Savage <psavage@redhat.com>
2015-09-15 11:59:50 +0100

    Release version 1.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="merkyl",
-    version='1.0.5',
+    version='1.0.6',
     description="Log file tailer with bottle",
     author="Pete Savage",
     keywords=["tail", "linux", "bottle"],
